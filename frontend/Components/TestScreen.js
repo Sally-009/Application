f713e3ce11752bfe3e5a82ac4e876c1ff817e536
Sally-009 <skojima2025@gmail.com>
2024-10-15 10:25:55 -0500

    added navigation to Welcome page from test screen

--- frontend/Components/TestScreen.js
+++ frontend/Components/TestScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { View, Text, SafeAreaView, Button, StyleSheet } from 'react-native';
 
-export default function TestScreen() {
+export default function TestScreen({ navigation }) {
+
+    const  navigateToWelcomeScreen = () => {
+        navigation.navigate('Welcome');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.headerText}>Test Screen</Text>
+            <Button style={styles.button} title="Go to Welcome Screen" onPress={navigateToWelcomeScreen} />
         </SafeAreaView>
     );
 }
